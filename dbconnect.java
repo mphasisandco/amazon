442f22f35f62c14274c07e801bad9e45ae1d620a
udhaysaran1 <awsudhaya.kumar@gmail.com>
2021-04-11 05:36:54 +0530

    stash 3 commit

--- dbconnect.java
+++ dbconnect.java
@@ -3,8 +3,5 @@ this is the second line in dbconnect.java - db2
 this is the third line in dbconnect.java - db3
 this is the fifth line in dnconnect.java -db5
 this is the fourth line in dbconnect.java -db4
-<<<<<<< Updated upstream
 this is the sizth line in dnconnect.java -db6
-=======
 this is the seventh lin in dnconnect.java -db7
->>>>>>> Stashed changes
